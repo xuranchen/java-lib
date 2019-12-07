bcaec9b0c9d06b6398bc28614e6ccad4d79276b5
basilisk487 <vasily@wavefront.com>
2019-12-06 21:53:18 -0600

    PUB-149: Streamline source tag processing for queue refactoring

--- java-lib/src/main/java/com/wavefront/api/SourceTagAPI.java
+++ java-lib/src/main/java/com/wavefront/api/SourceTagAPI.java
@@ -9,7 +9,6 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -25,35 +24,30 @@ public interface SourceTagAPI {
    * Add a single tag to a source.
    *
    * @param id       source ID.
-   * @param token    authentication token.
    * @param tagValue tag to add.
    */
   @PUT
   @Path("v2/source/{id}/tag/{tagValue}")
   @Produces(MediaType.APPLICATION_JSON)
   Response appendTag(@PathParam("id") String id,
-                     @QueryParam("t") String token,
                      @PathParam("tagValue") String tagValue);
 
   /**
    * Remove a single tag from a source.
    *
    * @param id       source ID.
-   * @param token    authentication token.
    * @param tagValue tag to remove.
    */
   @DELETE
   @Path("v2/source/{id}/tag/{tagValue}")
   @Produces(MediaType.APPLICATION_JSON)
   Response removeTag(@PathParam("id") String id,
-                     @QueryParam("t") String token,
                      @PathParam("tagValue") String tagValue);
 
   /**
    * Sets tags for a host, overriding existing tags.
    *
    * @param id             source ID.
-   * @param token          authentication token.
    * @param tagValuesToSet tags to set.
    */
   @POST
@@ -61,15 +55,12 @@ public interface SourceTagAPI {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   Response setTags(@PathParam ("id") String id,
-                   @QueryParam("t") String token,
                    List<String> tagValuesToSet);
-
 
   /**
    * Set description for a source.
    *
    * @param id          source ID.
-   * @param token       authentication token.
    * @param description description.
    */
   @POST
@@ -77,18 +68,15 @@ public interface SourceTagAPI {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   Response setDescription(@PathParam("id") String id,
-                          @QueryParam("t") String token,
                           String description);
 
   /**
    * Remove description from a source.
    *
    * @param id    source ID.
-   * @param token authentication token.
    */
   @DELETE
   @Path("v2/source/{id}/description")
   @Produces(MediaType.APPLICATION_JSON)
-  Response removeDescription(@PathParam("id") String id,
-                             @QueryParam("t") String token);
+  Response removeDescription(@PathParam("id") String id);
 }
