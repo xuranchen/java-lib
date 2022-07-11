de1b0ed34c725b46fc5cd71839f1a0998384fb36
Sean Chen <seanch@vmware.com>
2022-07-11 16:29:17 -0400

    address comments

--- java-lib/src/main/java/com/wavefront/ingester/JsonParser.java
+++ java-lib/src/main/java/com/wavefront/ingester/JsonParser.java
@@ -6,34 +6,46 @@ import javax.annotation.Nonnull;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Parses Json inputs to parsable format
+ */
 public class JsonParser {
     private final Map<String, Object> input;
-    private final String DELIMITER = "_";
+    private final String UNDERSCORE = "_";
     private final List<String> ignoreFlatten;
 
+    /***
+     * @param ignoreFlatten list of strings that will not be flattened
+     */
     public JsonParser(@Nonnull Map<String, Object> input, List<String> ignoreFlatten) {
         this.input = input;
         this.ignoreFlatten = ignoreFlatten;
     }
 
+    /**
+     * flattens nested json
+     * a : {b : x} -> a_b = x
+     * a:[x, y] -> a_0 = x, a_1 = y
+     * @param annotations annotations list to insert the final objects
+     */
     public void flattenJson(List<Annotation> annotations) {
         for (Map.Entry<String, Object> tagKV : input.entrySet()) {
-            recursiveHelper(tagKV.getKey(), tagKV.getValue(), annotations);
+            flattenJsonInternal(tagKV.getKey(), tagKV.getValue(), annotations);
         }
     }
 
-    public void recursiveHelper(String key, Object value, List<Annotation> annotations) {
+    private void flattenJsonInternal(String key, Object value, List<Annotation> annotations) {
         if (value == null) {
             annotations.add(Annotation.newBuilder().setKey(key).setValue("null").build());
         } else if (ignoreFlatten.contains(key) || value instanceof String) {
             annotations.add(Annotation.newBuilder().setKey(key).setValue(value.toString()).build());
         } else if (value instanceof Map<?, ?>) {
             for (Map.Entry<?, ?> tagKV : ((Map<?, ?>)value).entrySet()) {
-                recursiveHelper(concatonate(key, tagKV.getKey().toString()), tagKV.getValue(), annotations);
+                flattenJsonInternal(concatonate(key, tagKV.getKey().toString()), tagKV.getValue(), annotations);
             }
         } else if (value instanceof List) {
             for (int i = 0; i < ((List<?>) value).size(); i++) {
-                recursiveHelper(concatonate(key, String.valueOf(i)), ((List<?>) value).get(i), annotations);
+                flattenJsonInternal(concatonate(key, String.valueOf(i)), ((List<?>) value).get(i), annotations);
             }
         } else {
             annotations.add(Annotation.newBuilder().setKey(key).setValue(value.toString()).build());
@@ -41,6 +53,6 @@ public class JsonParser {
     }
 
     private String concatonate(String s1, String s2) {
-        return s1 + DELIMITER + s2;
+        return s1 + UNDERSCORE + s2;
     }
 }
