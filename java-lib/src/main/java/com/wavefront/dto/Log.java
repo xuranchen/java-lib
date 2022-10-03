2f0c1a99b77ea637baa5c12d0e7990abbd29435b
xuranchen <seanch@vmware.com>
2022-10-03 17:16:20 -0400

    MONIT-30933: Backlog deserialization  (#129)

--- java-lib/src/main/java/com/wavefront/dto/Log.java
+++ java-lib/src/main/java/com/wavefront/dto/Log.java
@@ -1,18 +1,10 @@
 package com.wavefront.dto;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import org.joda.time.Instant;
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import wavefront.report.Annotation;
 import wavefront.report.ReportLog;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
@@ -26,6 +18,7 @@ import static com.wavefront.common.SerializerUtils.appendQuoted;
  * @author amitw@vmware.com
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonDeserialize(using = LogDeserializer.class)
 public class Log implements Serializable {
 
     @JsonProperty("timestamp")
